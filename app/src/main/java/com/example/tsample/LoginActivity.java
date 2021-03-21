c155cac083039e053356981b687d7d41c6b4f153
NaiStlaw <32583325+NaiStlaw@users.noreply.github.com>
2021-03-20 22:21:01 -0400

    Initial commit

---
+++ app/src/main/java/com/example/tsample/LoginActivity.java
@@ -0,0 +1,50 @@
+package com.example.tsample;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+//import android.widget.Toolbar;
+
+import org.w3c.dom.Text;
+
+public class LoginActivity extends AppCompatActivity {
+    private Toolbar toolbar;
+    private EditText loginemail, loginPwd;
+    private Button loginBtn;
+    private TextView loginQn;
+    private Object Toolbar;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_login);
+
+
+        toolbar =  (Toolbar) findViewById(R.id.loginToolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle("Login");
+
+        loginemail = findViewById(R.id.loginEmail);
+        loginPwd = findViewById(R.id.loginPassword);
+        loginBtn = findViewById(R.id.loginButton);
+        loginQn = findViewById(R.id.loginPageQuestion);
+
+        loginQn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(LoginActivity.this, RegistrationActivity.class);
+                startActivity(intent);
+            }
+
+        });
+
+    }
+}
